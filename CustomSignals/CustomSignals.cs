99ae0d4df56b446e44276ffe1ad002caba48c9d4
London Banner <vbanne2@lsu.edu>
2024-11-01 17:37:08 -0500

    Updated Inventory Some More
    Added removed button functionality, fixed various issues.

--- CustomSignals/CustomSignals.cs
+++ CustomSignals/CustomSignals.cs
@@ -1,8 +1,12 @@
 using Godot;
 using System;
-
+/*
+Summary:
+This class serves as a hub for all custom signals that need to be used.
+*/
 public partial class CustomSignals : Node
 {
+	//Signal that fires when the inventory is updated in any way.
 	[Signal]
 	public delegate void InventoryUpdatedEventHandler();
 }
