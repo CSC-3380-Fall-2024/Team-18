d219b19c6397b980b0b9adc9e78442f7e9a42f34
PileOfRats <rbrea28@lsu.edu>
2024-11-02 18:27:04 -0500

    Hotfix to at least get project to build again
    See changes to main. Some functions called for member functions that didnt exist. Also had to change .text to .Text to align with Godot's internal members.
    
    Honestly have no idea what the .Connect problem was. Chatgpt helped me out with that error code CS1501.
    
    Getting no errors on build currently

--- Scripts/Npc.cs
+++ Scripts/Npc.cs
@@ -3,25 +3,25 @@ using System;
 
 
 public partial class Npc: CharacterBody2D{
-    public AnimatedSprite2D char_anim ;
-    
-    [Export] public Boolean talking = false;
-    
-    bool player_in_range = false;
-    [Export] public CanvasLayer dialoguebox;
+	public AnimatedSprite2D char_anim ;
+	
+	[Export] public Boolean talking = false;
+	
+	bool player_in_range = false;
+	[Export] public CanvasLayer dialoguebox;
 
-    [Export] public CanvasLayer Presstalk;
-    
-    public override void _Ready(){
-            char_anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-            dialoguebox = GetNode<CanvasLayer>("dialoguebox");
-            Presstalk = GetNode<CanvasLayer>("Presstalk");
-            char_anim.Play("idle");
+	[Export] public CanvasLayer Presstalk;
+	
+	public override void _Ready(){
+			char_anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+			dialoguebox = GetNode<CanvasLayer>("dialoguebox");
+			Presstalk = GetNode<CanvasLayer>("Presstalk");
+			char_anim.Play("idle");
 
-    }
+	}
 
-    
-    public void OnArea2DBodyEntered(Player body)
+	
+	public void OnArea2DBodyEntered(Player body)
 	{
 		//checks for the player specifically
 		if(body.IsInGroup("Player"))
@@ -31,7 +31,7 @@ public partial class Npc: CharacterBody2D{
 		}
 	}
 
-    public void OnArea2DBodyExited(Player body)
+	public void OnArea2DBodyExited(Player body)
 	{
 		if(body.IsInGroup("Player"))
 		{
@@ -40,24 +40,24 @@ public partial class Npc: CharacterBody2D{
 		}
 	}
 
-    public void UpdateAnimations(){
-            if(talking == true){
-                char_anim.Play("talk");
-            }
-            else{
-                char_anim.Play("idle");
-            }
-    }
+	public void UpdateAnimations(){
+			if(talking == true){
+				char_anim.Play("talk");
+			}
+			else{
+				char_anim.Play("idle");
+			}
+	}
 
-    public void Talk(InputEvent @event)
+	public void Talk(InputEvent @event)
 	{
 		if(@event.IsActionPressed("talk") && player_in_range == true)
 		{
 			talking = true;
-            dialoguebox.Visible = !dialoguebox.Visible;
-            Presstalk.Visible = !Presstalk.Visible;
-            GD.Print("Enter.");
-            
+			dialoguebox.Visible = !dialoguebox.Visible;
+			Presstalk.Visible = !Presstalk.Visible;
+			GD.Print("Enter.");
+			
 		}
 
 	}
