00e8da851a5b87d29c3599d0cbecaa7141fee67b
PileOfRats <rbrea28@lsu.edu>
2024-11-02 20:40:08 -0500

    Please look over this
    Tiny fixes here, please look over the dialogue system because I have no idea what's going on there.
    
    Please and thank you. I can't even get the signals to connect from Area2D for some reason, despite it working for everything else

--- Scripts/Npc.cs
+++ Scripts/Npc.cs
@@ -10,14 +10,26 @@ public partial class Npc: CharacterBody2D{
 	bool player_in_range = false;
 	[Export] public CanvasLayer dialoguebox;
 
-	[Export] public CanvasLayer Presstalk;
+	//[Export] public CanvasLayer Presstalk;
 	
 	public override void _Ready(){
 			char_anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 			dialoguebox = GetNode<CanvasLayer>("dialoguebox");
-			Presstalk = GetNode<CanvasLayer>("Presstalk");
+			//Presstalk = GetNode<CanvasLayer>("Presstalk");
 			char_anim.Play("idle");
+			dialoguebox.Visible = false;
+			
+			//Presstalk.Visible = false;
 
+	}
+	/* 
+	THIS IS JUST TO TEST. PLEASE REMOVE IF YOU GET WORKING
+	*/
+	public override void _Process(double delta){
+		if (player_in_range && Input.IsActionJustPressed("ui_add"))
+		{
+			GD.Print("hello for now");
+		}
 	}
 
 	
@@ -26,8 +38,9 @@ public partial class Npc: CharacterBody2D{
 		//checks for the player specifically
 		if(body.IsInGroup("Player"))
 		{
+			GD.Print("yes");
 			player_in_range = true;
-			body.interact_ui.Visible = true;
+			body.Presstalk.Visible = true;
 		}
 	}
 
@@ -35,8 +48,9 @@ public partial class Npc: CharacterBody2D{
 	{
 		if(body.IsInGroup("Player"))
 		{
+			GD.Print("no");
 			player_in_range = false;
-			body.interact_ui.Visible = false;
+			body.Presstalk.Visible = false;
 		}
 	}
 
@@ -53,9 +67,10 @@ public partial class Npc: CharacterBody2D{
 	{
 		if(@event.IsActionPressed("talk") && player_in_range == true)
 		{
+			GD.Print("Please god help me.");
 			talking = true;
 			dialoguebox.Visible = !dialoguebox.Visible;
-			Presstalk.Visible = !Presstalk.Visible;
+			//Presstalk.Visible = !Presstalk.Visible;
 			GD.Print("Enter.");
 			
 		}
